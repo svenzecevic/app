9cb5278e83313bbc1b4a5d0e0eff521feb508ea7
sven_z <sven.zecevic@gmail.com>
2020-02-29 18:40:19 +0100

    style modification (work style)

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,24 +3,26 @@ import "./index.css";
 
 function Pagination() {
   return (
-    <div class="pagination">
-      <ul>
-        <li class="list-items">
-          <a href="#">1</a>
-        </li>
-        <li class="list-items">
-          <a href="#">2</a>
-        </li>
-        <li class="list-items">
-          <a href="#">3</a>
-        </li>
-        <li class="list-items">
-          <a href="#">4</a>
-        </li>
-        <li class="list-items">
-          <a href="#">5</a>
-        </li>
-      </ul>
+    <div class="center">
+      <div class="pagination">
+        <ul>
+          <li class="list-items">
+            <a href="#">1</a>
+          </li>
+          <li class="list-items">
+            <a href="#">2</a>
+          </li>
+          <li class="list-items">
+            <a href="#">3</a>
+          </li>
+          <li class="list-items">
+            <a href="#">4</a>
+          </li>
+          <li class="list-items">
+            <a href="#">5</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
